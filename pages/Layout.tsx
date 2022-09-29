e48a9ab4df7b941290478f8f2725b4b3bd2bfbf2
viniciusgit2 <4xv6patelfin@gmail.com>
2022-09-29 11:25:14 -0300

    tudo certo mas incompleto

---
+++ pages/Layout.tsx
@@ -0,0 +1,10 @@
+import Titulo from "./Titulo"
+export default function  layout (){
+    return(
+    <div>
+     <div className={'flex col-w2/3 bg-orange-700, text bg-black'}></div>
+     <Titulo/>
+     
+ </div>
+    )
+}
