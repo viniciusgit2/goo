c2b1ba61584355e3a293aad9889e96463e71a3f6
viniciusgit2 <4xv6patelfin@gmail.com>
2022-10-03 11:12:14 -0300

    tudo certo

---
+++ pages/Tabela.tsx
@@ -0,0 +1,5 @@
+import Titulo from "./Titulo"
+import Cliente from "./Cliente"
+export default function tabela(){
+return <table> <Titulo/><tr><th>id</th>nome<th></th>idade</tr><Cliente/> </table>
+} 
